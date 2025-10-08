29c99e9d1a5468ee44f58bdeedc1beeeb10d857d
Igor <igor.mamaev1@gmail.com>
2025-10-08 19:47:39 +0300

    fix misuse of try_register_user

--- handlers/start.py
+++ handlers/start.py
@@ -9,5 +9,5 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_command(message: Message) -> None:
-    db.try_register_user(message.chat.id, message.from_user.id, message.from_user.username)
+    db.try_register_user(message.chat.id, message.from_user.username, message.from_user.id)
     await message.answer(text="Hello, world!")
