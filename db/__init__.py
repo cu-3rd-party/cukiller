132f82a243c4c47af0b62b430fbec3d114aa4f48
Mamaev Igor <77161122+pxc1984@users.noreply.github.com>
2025-10-08 19:17:28 +0300

    Initial commit

---
+++ db/__init__.py
@@ -0,0 +1,1 @@
+from .backend import try_register_user
---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+import db
+
+start_router = Router()
+
+@start_router.message(CommandStart())
+async def start_command(message: Message) -> None:
+    db.try_register_user(message.chat.id, message.from_user.id, message.from_user.username)
+    await message.answer(text="Hello, world!")
