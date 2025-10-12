b9360b0fa33fa8190cdbc07973146d28c67b80c8
Igor Mamaev <igor.mamaev1@gmail.com>
2025-10-12 13:54:31 +0000

    Run Ruff format

--- tgbot/services/admin_chat.py
+++ tgbot/services/admin_chat.py
@@ -5,7 +5,9 @@ from aiogram import Bot
 from tgbot.config import Config
 
 
-async def send_message(text: str, bot: Bot, config: Config, tag: Optional[str] = None):
+async def send_message(
+    text: str, bot: Bot, config: Config, tag: Optional[str] = None
+):
     await bot.send_message(
         chat_id=config.tg_bot.admin_chat_id,
         text=text if not tag else f"#{tag}\n\n{text}",
