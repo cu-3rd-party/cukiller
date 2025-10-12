b73ef9daf877325d4bf5d94815f9f07f5402d7fb
Igor <igor.mamaev1@gmail.com>
2025-10-12 10:36:40 +0300

    autoimport

--- bot.py
+++ bot.py
@@ -6,10 +6,8 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from tgbot.config import load_config
-# TODO: переписать на importlib
-from tgbot.handlers.admin import router as admin_router
-from tgbot.handlers.echo import router as echo_router
-from tgbot.handlers.start import router as start_router
+import os
+import importlib
 from tgbot.middlewares.environment import EnvironmentMiddleware
 
 logger = logging.getLogger(__name__)
@@ -20,11 +18,33 @@ def register_all_middlewares(dp, config):
 
 
 def register_all_handlers(dp):
-    dp.include_routers(
-        admin_router,
-        echo_router,
-        start_router
-    )
+    """
+    Очень сложная (нет) логика для автоматического импорта хэндлеров.
+    Каждый хэндлер ОБЯЗАН содержать в себе router = Router(), который уже и будет подвязываться к нашему диспетчеру
+    :param dp:
+    :return:
+    """
+    base_package = "tgbot.handlers"
+    base_path = os.path.join("tgbot", "handlers")
+    routers = []
+
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if not file.endswith(".py") or file == "__init__.py":
+                continue
+
+            rel_path = os.path.relpath(root, base_path).replace(os.sep, ".")
+            module_name = file[:-3]
+
+            if rel_path == ".":
+                full_module_name = f"{base_package}.{module_name}"
+            else:
+                full_module_name = f"{base_package}.{rel_path}.{module_name}"
+
+            module = importlib.import_module(full_module_name)
+            routers.append(module.router)
+
+    dp.include_routers(*routers)
 
 
 def setup_django():
