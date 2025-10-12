9237cfce8c58a43a1718a1419aa430281af5ec6f
Igor <igor.mamaev1@gmail.com>
2025-10-12 15:13:10 +0300

    экспериментальное изменение

--- tgbot/services/admin_chat.py
+++ tgbot/services/admin_chat.py
@@ -4,6 +4,14 @@ from tgbot.config import Config
 
 
 async def send_admin_chat_message(text: str, bot: Bot, config: Config):
+    # отправляем всем админам в лс
+    for chat_id in config.tg_bot.admin_ids:
+        await bot.send_message(
+            chat_id=chat_id,
+            text=text,
+            parse_mode="HTML",
+        )
+    # дублируем в общую группу
     await bot.send_message(
         chat_id=config.tg_bot.admin_chat_id,
         text=text,
