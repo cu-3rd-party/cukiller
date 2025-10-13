8abbec534ba1c48a9a5687d6de8a44300830bdfb
dreamyranger <dreamswanderer@tutanota.com>
2025-10-13 10:38:58 +0300

    init chat

--- db/main.py
+++ db/main.py
@@ -2,6 +2,7 @@ import logging
 
 from tortoise import Tortoise
 
+from db.models import Chat
 from settings import Settings
 
 logger = logging.getLogger(__name__)
@@ -15,6 +16,7 @@ async def init_db(settings: Settings) -> None:
     if settings.tortoise_generate_schemas:
         await Tortoise.generate_schemas()
         logger.info("Генерация схем Tortoise ORM выполнена")
+    await _ensure_default_admin_chat()
 
 
 async def close_db() -> None:
@@ -22,3 +24,18 @@ async def close_db() -> None:
 
     await Tortoise.close_connections()
     logger.info("Tortoise ORM соединения закрыты")
+
+
+async def _ensure_default_admin_chat() -> None:
+    defaults = {
+        "chat_id": -1003148190868,
+        "name": "Admin Logs",
+        "type": "supergroup",
+        "purpose": "Логи админов",
+    }
+
+    _, created = await Chat.get_or_create(key="logs", defaults=defaults)
+    if created:
+        logger.info(
+            "Создан системный чат 'logs'",
+        )
