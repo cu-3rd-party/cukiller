d6abbd34f5cc1e3587dad46a7d5f6f70fb71f740
Igor <igor.mamaev1@gmail.com>
2025-10-15 23:09:37 +0300

    set admin commandslist

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, BotCommand
 
 from bot.filters.admin import AdminFilter
 
@@ -8,5 +8,11 @@ router = Router()
 
 
 @router.message(AdminFilter(), CommandStart())
-async def admin_start(message: Message):
-    await message.reply("Привет, админ!")
+async def admin_start(message: Message, bot: Bot):
+    await bot.set_my_commands(commands=[
+        BotCommand(command="/start", description="Начать работу с ботом"),
+    ])
+    await message.reply(text=(
+        "Привет, админ!\n"
+        "Добавил тебе список команд в менюшку, полюбуйся)\n"
+    ))
