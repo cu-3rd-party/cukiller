7457885df3d75256c172a6e5c9eed0432b5d5bdf
Igor <igor.mamaev1@gmail.com>
2025-10-15 23:17:49 +0300

    admin panel is now unique to one admin

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,6 +1,11 @@
 from aiogram import Router, Bot
 from aiogram.filters import CommandStart
-from aiogram.types import Message, BotCommand
+from aiogram.types import (
+    Message,
+    BotCommand,
+    BotCommandScope,
+    BotCommandScopeChat,
+)
 
 from bot.filters.admin import AdminFilter
 
@@ -9,10 +14,15 @@ router = Router()
 
 @router.message(AdminFilter(), CommandStart())
 async def admin_start(message: Message, bot: Bot):
-    await bot.set_my_commands(commands=[
-        BotCommand(command="/start", description="Начать работу с ботом"),
-    ])
-    await message.reply(text=(
-        "Привет, админ!\n"
-        "Добавил тебе список команд в менюшку, полюбуйся)\n"
-    ))
+    await bot.set_my_commands(
+        commands=[
+            BotCommand(command="/start", description="Начать работу с ботом"),
+        ],
+        scope=BotCommandScopeChat(chat_id=message.chat.id),
+    )
+    await message.reply(
+        text=(
+            "Привет, админ!\n"
+            "Добавил тебе список команд в менюшку, полюбуйся)\n"
+        )
+    )
