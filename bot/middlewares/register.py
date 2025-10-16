0cece836432b1eb6d1870a7ddf8f1cb975e314ee
Mamaev Igor <77161122+pxc1984@users.noreply.github.com>
2025-10-16 17:52:46 +0300

    Registration middleware (#22)
    * register user middleware
    
    * use dp.update instead of dp.message
    
    * Run Ruff format

---
+++ bot/middlewares/register.py
@@ -0,0 +1,37 @@
+import logging
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject, Update
+
+from db.models import User
+
+
+logger = logging.getLogger(__name__)
+
+
+class RegisterUserMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        super().__init__()
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: Update,
+        data: dict[str, Any],
+    ):
+        user = event.message.from_user
+        user, created = await User().update_or_create(
+            tg_id=user.id,
+            defaults={
+                "tg_username": user.username,
+                "given_name": user.first_name,
+                "family_name": user.last_name,
+            },
+        )
+        data["user"] = user
+        if created:
+            logger.info(f"New user with telegram id: {user.tg_id}")
+
+        return await handler(event, data)
