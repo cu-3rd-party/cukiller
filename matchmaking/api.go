63b16d6890a17e31343b44c8d3080a4e9ed93d00
Igor <igor.mamaev1@gmail.com>
2025-10-31 21:06:11 +0300

    adding players to queues

--- matchmaking/api.go
+++ matchmaking/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,6 +11,8 @@ import (
 func startupHttp() {
 	http.HandleFunc("/ping/", ping)
 	http.HandleFunc("/health/", health)
+	http.HandleFunc("/add/killer/", addKiller)
+	http.HandleFunc("/add/victim/", addVictim)
 
 	addr := fmt.Sprintf(":%d", conf.Port)
 	logger.Info("HTTP server starting on %s", addr)
@@ -23,7 +26,7 @@ func startupHttp() {
 func ping(w http.ResponseWriter, r *http.Request) {
 	logger.Info("Ping endpoint called from %s", r.RemoteAddr)
 
-	_, err := fmt.Fprintf(w, "Hello, world!\n")
+	_, err := fmt.Fprintf(w, "ok\n")
 	if err != nil {
 		logger.Error("Failed to write ping response: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -45,4 +48,41 @@ func health(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+var KillerPool = make(map[int]QueuePlayer)
+var VictimPool = make(map[int]QueuePlayer)
+
+func addKiller(w http.ResponseWriter, r *http.Request) {
+	// 	1. parse PlayerData from request
+	var data PlayerData
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		_, _ = w.Write([]byte("failed to parse PlayerData from json request"))
+		w.WriteHeader(400)
+	}
+	// 2. put player into killer queue
+	addPlayerToPool(KillerPool, data)
+	w.WriteHeader(201)
+}
+
+func addVictim(w http.ResponseWriter, r *http.Request) {
+	// 	1. parse PlayerData from request
+	var data PlayerData
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		_, _ = w.Write([]byte("failed to parse PlayerData from json request"))
+		w.WriteHeader(400)
+	}
+	// 2. put player into killer queue
+	addPlayerToPool(VictimPool, data)
+	w.WriteHeader(201)
+}
+
+func addPlayerToPool(pool map[int]QueuePlayer, data PlayerData) {
+	pool[data.TgId] = QueuePlayer{
+		TgId:       data.TgId,
+		JoinedAt:   time.Now(),
+		PlayerData: data,
+	}
 }
