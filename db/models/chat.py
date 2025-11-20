61b1125a49d3094b5a7ada8271cb980046ab6762
Igor Mamaev <igor.mamaev1@gmail.com>
2025-11-20 14:49:34 +0000

    Run Ruff format

--- db/models/chat.py
+++ db/models/chat.py
@@ -11,7 +11,7 @@ class Chat(TimestampedModel):
         table = "chats"
         table_description = "Админ-чаты Telegram"
         unique_together = (("chat_id", "key"),)
-        indexes = (("chat_id",),("key",))
+        indexes = (("chat_id",), ("key",))
 
     def __str__(self) -> str:
         return f"<Chat {self.chat_id}:{self.key}>"
