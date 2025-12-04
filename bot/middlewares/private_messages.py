08d7873e82ffb30218ffe7e391dca24f03a20ca1
Mamaev Igor <77161122+pxc1984@users.noreply.github.com>
2025-12-04 12:16:01 +0300

    Улучшенная /stats (#53)

--- bot/middlewares/private_messages.py
+++ bot/middlewares/private_messages.py
@@ -6,12 +6,17 @@ from aiogram.types import Message
 
 
 class PrivateMessagesMiddleware(BaseMiddleware):
+    async def __init__(self, *exclusions) -> None:
+        self.exclusions = exclusions
+
     async def __call__(
         self,
         handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
         event: Message,
         data: dict[str, Any],
     ) -> Any:
+        if event.text in self.exclusions:
+            return await handler(event, data)
         if event.from_user.id != event.chat.id:
             await event.answer("Этот бот работает только в личных сообщениях.")
             return None
